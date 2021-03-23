5981ee3919fb4121cd1495124cd8a3c24365d9bb
Michelle-Ward <Mward352@gmail.com>
2021-03-22 20:52:31 -0700

    set up mocha and chai testing enviroment

---
+++ spec/toyProblemSpec.js
@@ -0,0 +1,12 @@
+describe('Toy Problems', function() {
+
+  describe('bubble sort', function() {
+    it('should return an array', function() {
+      //call bubble sort function with array
+      // create expected variable
+      let actual = true;
+      let expected = true;
+      expect(actual).to.be.eql(expected);
+    });
+  });
+});
