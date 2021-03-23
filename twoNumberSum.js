148f7d27becf0d520ee4772acee9fd57a3479b16
Michelle-Ward <Mward352@gmail.com>
2021-03-23 14:06:37 -0700

    write IOCE and pseudocode for twoNumberSum

---
+++ twoNumberSum.js
@@ -0,0 +1,13 @@
+//i: array of integers, target - integer
+//o: return an array of two integers in input array that sum up to target
+//c: must return the first pair
+//e: not possible to reach target sum with given numbers? return empty array
+
+//iterate over the array, check all numbers after the current number to see if they equal target sum. If the pair does return a tupple with the current number and the current 2nd number. return an empty arrray at the end of iteration
+
+const twoNumberSum = (array, target) => {
+  //iterate over array
+    //iterate over array starting at first index
+      //if outer current number plus inner current number equals target sum
+  //return empty array
+}
