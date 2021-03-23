59813948bc1d3e6b0b6048b6115cd5c030e3337c
Michelle-Ward <Mward352@gmail.com>
2021-03-23 13:06:12 -0700

    add tests for bubbleSort

--- spec/toyProblemSpec.js
+++ spec/toyProblemSpec.js
@@ -1,11 +1,23 @@
 describe('Toy Problems', function() {
-
   describe('bubble sort', function() {
     it('should return an array', function() {
-      //call bubble sort function with array
-      // create expected variable
-      let actual = true;
-      let expected = true;
+      let actual = bubbleSort([ 9, 5, 4]);
+      let expected = [ 4, 5, 9];
+      expect(actual).to.be.eql(expected);
+    });
+    it('should be able to sort negative and positive integers', function() {
+      let actual = bubbleSort([ 54, 32, -12, 3, 24, -50]);
+      let expected = [ -50, -12, 3, 24, 32, 54];
+      expect(actual).to.be.eql(expected);
+    });
+    it('should return an empty array if input array is empty', function() {
+      let actual = bubbleSort([]);
+      let expected = [];
+      expect(actual.length).to.be.equal(expected.length);
+    });
+    it('should be able to sort negative numbers', function() {
+      let actual = bubbleSort([ -6, -34, -73, -2, -14]);
+      let expected = [ -2, -6, -14, -34, -73];
       expect(actual).to.be.eql(expected);
     });
   });
