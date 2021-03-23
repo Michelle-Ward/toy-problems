17b50d6d90268fcd79972c772dbf933b1bf92f5c
Michelle-Ward <Mward352@gmail.com>
2021-03-23 13:00:33 -0700

    write IOCE and pseudocode for bubbleSort

---
+++ bubbleSort.js
@@ -0,0 +1,19 @@
+//input: array of numbers (positive and negative integers)
+//output: sorted array least -> greatest
+//constraints: sort in place
+//edge cases: empty array? return empty array. assume only integer values in the array
+
+//keeping track if any swaps occur iterate over the input array. if the current number is greater than the next number, swap current and next. mark that a swap occured. when done iterating, if a swap occured call bubblesort again on the input array. return the array;
+
+const bubbleSort = (array) => {
+  //create variable swapped assign to false
+  //iterate over the array
+    //if the current number is greater than the next
+      //create temp variable to save current value
+      //assign current index to next value
+      //assign next index to temp
+      //reassign swapped variable to true
+  //if swapped
+    //call bubblesort on input array
+  //return input array
+}
