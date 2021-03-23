36fa8b606fdb15346b7cb3f5166d15a58e375f3f
Michelle-Ward <Mward352@gmail.com>
2021-03-23 13:56:32 -0700

    write tests for twoNumberSum

--- spec/toyProblemSpec.js
+++ spec/toyProblemSpec.js
@@ -1,5 +1,5 @@
 describe('Toy Problems', function() {
-  describe('bubble sort', function() {
+  describe('bubbleSort', function() {
     it('should return an array', function() {
       let actual = bubbleSort([ 9, 5, 4]);
       let expected = [ 4, 5, 9];
@@ -18,6 +18,28 @@ describe('Toy Problems', function() {
     it('should be able to sort negative numbers', function() {
       let actual = bubbleSort([ -6, -34, -73, -2, -14]);
       let expected = [-73, -34, -14, -6, -2];
+      expect(actual).to.be.eql(expected);
+    });
+  });
+  describe('twoNumberSum', function() {
+    it('should return an array', function() {
+      let actual = twoNumberSum([2,6,3,5,8,1], 8);
+      let expected = [2,6];
+      expect(actual).to.be.eql(expected);
+    });
+    it('should be able to return the first pair of numbers that equal the target sum', function() {
+      let actual = twoNumberSum([2,6,3,5,8,1, 4], 9);
+      let expected = [6,3];
+      expect(actual).to.be.eql(expected);
+    });
+    it('should be able to handle negative integers', function() {
+      let actual = bubbleSort([5,-3, 9, 1, 4, -1], 6);
+      let expected = [-3,9];
+      expect(actual.length).to.be.equal(expected.length);
+    });
+    it('should be able to handle a negative target input', function() {
+      let actual = twoNumberSum([-10, -2, -1, -5], -3);
+      let expected = [-2,-1];
       expect(actual).to.be.eql(expected);
     });
   });
