3c4fd516301f4ec7c350b27d6ddc70c2d4acdf5a
Michelle-Ward <Mward352@gmail.com>
2021-03-23 14:11:46 -0700

    implement twoNumberSum

--- twoNumberSum.js
+++ twoNumberSum.js
@@ -4,10 +4,18 @@
 //e: not possible to reach target sum with given numbers? return empty array
 
 //iterate over the array, check all numbers after the current number to see if they equal target sum. If the pair does return a tupple with the current number and the current 2nd number. return an empty arrray at the end of iteration
-
-const twoNumberSum = (array, target) => {
-  //iterate over array
+//iterate over array
     //iterate over array starting at first index
       //if outer current number plus inner current number equals target sum
   //return empty array
+
+const twoNumberSum = (array, target) => {
+  for ( let i = 0; i < array.length; i++ ) {
+    for ( let j = 0; j < array.length; j++ ) {
+      if ( array[i] + array[j] === target ) {
+        return [ array[i], array[j]];
+      }
+    }
+  }
+  return [];
 }
