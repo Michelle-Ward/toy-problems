b5b39b62368c15afc3957b99d42ce521d38b3e44
Michelle-Ward <Mward352@gmail.com>
2021-03-24 12:13:52 -0700

    implement isUnique

--- isUnique.js
+++ isUnique.js
@@ -5,11 +5,19 @@
 
 //keep track of duplicate values. iterate over string. if current value has been seen before return false. otherwise add the current value to the tracker with an initial value of 1 and continue. return true after iteration completes
 
-const isUnique = (string) => {
-  //create object to keep track of chars
+//create object to keep track of chars
   //iterate over string
     //if the object at current charater is defined
       //return false
     //other wise add it to obj assigned to 1
   //return true
+const isUnique = (string) => {
+  let charObj = {};
+  for ( let i = 0; i < string.length; i++ ) {
+    if ( charObj[string[i]] ) {
+      return false;
+    }
+    charObj[string[i]] = 1;
+  }
+  return true;
 }
