d1150cad34c86c1e2d7770b78e727cbd726f8ea5
Michelle-Ward <Mward352@gmail.com>
2021-03-29 10:47:36 -0700

    implement optimization with a single for loop and check for majority each iteration

--- majoritySearch.js
+++ majoritySearch.js
@@ -3,17 +3,23 @@
 //constraints: aim for linear time
 //edge: no majority? assume there is always a majority
 const majoritySearch = (array) => {
+  if (array.length === 1) {
+    return array[0];
+  }
   let charObj ={};
   for ( let i = 0; i < array.length; i++ ) {
     if ( charObj[array[i]] === undefined ) {
       charObj[array[i]] = 1;
     } else {
       charObj[array[i]] += 1;
+      if(charObj[array[i]] > (array.length / 2)) {
+        return array[i];
+      }
     }
   }
-  for  ( let key in charObj ) {
-    if ( charObj[key] > ( array.length / 2 )) {
-      return Number(key);
-    }
-  }
+  // for  ( let key in charObj ) {
+  //   if ( charObj[key] > ( array.length / 2 )) {
+  //     return Number(key);
+  //   }
+  // }
 }
