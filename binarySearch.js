7d0f6b726c9a7c1b1ef15ffd486588f48441f922
Michelle-Ward <Mward352@gmail.com>
2021-04-13 21:56:36 -0700

    implement binarySearch

--- binarySearch.js
+++ binarySearch.js
@@ -2,8 +2,8 @@
 //o: index of target num found or -1 if not
 //c: none
 //e:empty array? -1
-const binarySearch = (array, target) => {
-  //create left pointer assign to start of array
+
+//create left pointer assign to start of array
   //create right pointer assign to end of the array
   //while left is less than right
     //create mid assigned to right- left divided by 2  plus left and round down
@@ -14,4 +14,20 @@ const binarySearch = (array, target) => {
     //if the array at mid is more than the target
       //reassign right to mid minus 1
   //return -1
-}
+const binarySearch = (array, target) => {
+    let left = 0;
+    let right = array.length-1;
+    while (left <= right) {
+        let mid = Math.floor((right - left)/2 +left);
+        if (array[mid] === target) {
+            return mid;
+        }
+        if (array[mid] < target) {
+            left = mid+1;
+        }
+        if (array[mid] > target) {
+            right = mid-1;
+        }
+    }
+    return -1;
+};
