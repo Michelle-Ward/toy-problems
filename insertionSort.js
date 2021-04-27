96447b1ebe4216a8e4e808165af68db23cc7aae7
Michelle-Ward <Mward352@gmail.com>
2021-04-27 12:56:47 -0700

    write pseudocode for insertionSort

--- insertionSort.js
+++ insertionSort.js
@@ -1,7 +1,14 @@
-//i:
-//o:
-//c:
-//e:
-const insertionSort = (array) => {
 
+const insertionSort = (array) => {
+  //iterate over the array starting at first index
+    //create variable to hold index of current value to compare assigned to i intially
+    //iterate backward starting i - 1
+      //if the current i value is less than the current j value
+        //create temp to hold i value
+        //reassign i index to j value
+        //reassign j index to temp value
+        //reassign variable index of value to compare to j
+        //continue iterating
+      //no longer swapping values break the inner loop
+  //return the sorted array
 }
