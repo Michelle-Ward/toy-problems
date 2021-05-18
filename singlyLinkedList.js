b04521a1f02701c4919d15659c934fe7831742fa
Michelle-Ward <Mward352@gmail.com>
2021-05-18 10:31:03 -0700

    create construtors for node and list, also pseudocode for adding to tail

---
+++ singlyLinkedList.js
@@ -0,0 +1,23 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor(){
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(value) {
+    //create a new node with value
+    //if head and tail are null
+      //reassign both to the new node
+    //else
+      //reassign the current tail's next to the new node
+      //reassign tail to the new node
+  }
+}
